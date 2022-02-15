1aa70271d0cacd2f7027da88e68b4af8cf53827f
Aman Sharma <amansharmalfs5@gmail.com>
2022-02-15 12:54:01 +0530

    Add files via upload

---
+++ IP App/Models/Plan_Details.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IP_App.Models
+{
+    public class Plan_Details
+    {
+        [Required]
+        public int Plan_ID { get; set; }
+        [Required]
+        [Display(Name ="Plan")]
+        public string Plan_Name { get; set; }
+        [Required]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Only numbers allowed")]
+        public Nullable<int> Amount { get; set; }
+        [Required]
+        [Display(Name = "FUP(in GB)")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Only numbers allowed")]
+        public Nullable<int> Usage_Limit { get; set; }
+        [Required]
+        [Display(Name = "Speed(in Mbps)")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Only numbers allowed")]
+        public Nullable<int> Speed { get; set; }
+        [Required]
+        [Display(Name = "Renewal Duration(in Days)")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Only numbers allowed")]
+        public Nullable<int> Renewal_Duration { get; set; }
+
+        [Required]
+        [Display(Name = "Additional Subscriptions")]
+        public string Subscriptions_Included { get; set; }
+        [Required]
+        [Display(Name = "Type(Prepaid/Postpaid)")]
+        public string Plan_Type { get; set; }
+        [Required]
+        [Display(Name = "Installation Charges")]
+        public Nullable<int> Installation_Charges { get; set; }
+    }
+}
---
+++ IP App/Models/UserDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IP_App.Models
+{
+    public class UserDetails
+    {
+        [Required]
+        public int User_ID { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        [Display(Name ="First Name")]
+        public string User_First_Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        [Display(Name = "Last Name")]
+        public string User_Last_Name { get; set; }
+        [Required]
+        [Display(Name = "Email ID")]
+        public string User_Emaild { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        public string User_Password { get; set; }
+    }
+}
